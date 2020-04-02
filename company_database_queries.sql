c8a5de6e60bafd02df6ff80d29ae1339d9837414
Ben <bnaultmaurer@gmail.com>
2020-04-01 19:02:50 -0500

    add queries as part of the nested queries subject.

--- company_database_queries.sql
+++ company_database_queries.sql
@@ -132,7 +132,30 @@ FROM employee
 JOIN branch
 ON employee.emp_id = branch.mgr_id;
 
--- Nested Queries
+-- Nested Queries (3:12:05)
+
+-- find names of all employees who have
+-- sold over $30,000 to a single client
+SELECT employee.first_name, employee.last_name
+FROM employee
+WHERE employee.emp_id IN (
+    SELECT works_with.emp_id
+    FROM works_with
+    WHERE works_with.total_sales > 30000
+);
+
+-- find all cients who are handled by the branch
+-- that Michael Scott manages
+SELECT client.client_name
+FROM client
+WHERE client.branch_id = (
+    SELECT branch.branch_id
+    FROM branch
+    WHERE branch.mgr_id = 102
+    LIMIT 1
+);
+
+
 
 
 
